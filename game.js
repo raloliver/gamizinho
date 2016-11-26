0c82d84df18f82e59a4f237975d61fd06a5835f7
Ral <contato@raloliver.com.br>
2016-11-26 09:44:09 -0200

    babel watch ok

---
+++ game.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var num1 = 2;
+var num2 = '3';
+document.write(num1 + Number(num2) + '<br>');
+document.write(num1 + +num2);
+
+/*
+let num = 1.123;
+let num2 = num.toFixed(2);
+//aqui o método converter o número numa string
+document.write(typeof num2);
+*/
+
+/*
+let a = 1; {
+    let b = 2;
+    document.write(b);
+}
+document.write(a);
+*/
+
+//document.write(1);
